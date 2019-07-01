57d7dc9bccbc41101d037d76fca038b982fa5d23
Anthony Anyanwu <anthony.anyanwu@andela.com>
2019-07-01 11:10:46 +0100

    Started fleshing out getTag(...)

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -58,3 +58,5 @@ class Accordion extends BaseComponent {
         }
     }
 }
+
+module.exports = Accordion;
---
+++ tasks/scanCtags.js
@@ -0,0 +1,35 @@
+
+const gulp = require('gulp');
+const fs = require('fs');
+const path = require('path');
+
+gulp.task('scanCtags', (cb) => {
+  const components = path.join(path.dirname(fs.realpathSync(__filename)), '../src/components');
+
+  fs.readdir(components, (_err, files) => {
+    const scripts = [];
+
+    files.forEach((file) => {
+      const dir = path.join(components, file);
+      if (fs.lstatSync(dir).isDirectory()) {
+        scripts.push(path.join(dir, 'index.js'));
+      }
+    });
+
+    // console.log(scripts);
+
+    // Load base.js
+    // eslint-disable-next-line global-require
+    // eslint-disable-next-line import/no-dynamic-require
+    const BaseComponent = fs.readFileSync(path.join(components, 'base.js'), 'utf8');
+
+    scripts.forEach((script) => {
+      const data = fs.readFileSync(script, 'utf8');
+      // eslint-disable-next-line no-eval
+      const ComponentClass = eval(BaseComponent + data);
+      console.log(new ComponentClass(null, null, false).tagName());
+    });
+    
+    cb();
+  });
+});
