ee05a5b49b3a9569e2c8e70d41d98ffbb6987690
Anthony Anyanwu <anthony.anyanwu@andela.com>
2020-05-15 12:51:36 +0100

    Add support for conditionals | Add support synthetic contexts | Add invocation caching to synthetic invocation

--- src/components/antelope/index.js
+++ src/components/antelope/index.js
@@ -1,5 +1,4 @@
 class Antelope extends BaseComponent {
-
     getType() {
         return BaseComponent.VISUAL_COMPONENT_TYPE;
     }
@@ -17,12 +16,69 @@ class Antelope extends BaseComponent {
         return this.tagName(c);
     }
 
+    // eslint-disable-next-line no-underscore-dangle
+    _capitalize(name) {
+        return name.toUpperCase();
+    }
+
     getComponentId() {
         return this.id;
     }
 
     sayHello(name, age) {
         return `Hello ${name}. You are ${age} years old`;
+    }
+
+    // eslint-disable-next-line no-unused-vars
+    dynamicUsers(people) {
+        // console.log(JSON.stringify(people));
+        return [
+            {
+                id: 1234,
+                books: [
+                    {
+                        name: 'Angels and Demons',
+                        releaseYear: 1994,
+                        authors: ['John Kelly', 'Amber Rose'],
+                    },
+                    {
+                        name: 'The Red man',
+                        releaseYear: 1994,
+                        authors: ['Peter Clinch', 'James Ivy'],
+                    },
+                ],
+            },
+            {
+                id: 5678,
+                books: [
+                    {
+                        name: 'Co and Theirs',
+                        releaseYear: 2005,
+                        authors: ['Abnel Loka', 'Jiji Kuku'],
+                    },
+                    {
+                        name: 'Live Trail',
+                        releaseYear: 2007,
+                        authors: ['Joe Poe', 'Isa Delaware'],
+                    },
+                ],
+            },
+            {
+                id: 8265,
+                books: [
+                    {
+                        name: 'Girls and Buys',
+                        releaseYear: 1245,
+                        authors: ['Jogn Bol', 'Koko Haj'],
+                    },
+                    {
+                        name: 'Mirange',
+                        releaseYear: 2000,
+                        authors: ['James Edward', 'Peter Tucker'],
+                    },
+                ],
+            },
+        ];
     }
 
     /**
